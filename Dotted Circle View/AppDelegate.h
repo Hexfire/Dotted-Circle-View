da6a9487ba873a4297828817cbafd8e47b67ba1a
Hamish Knight <hk@southerncare.co.uk>
2016-03-12 19:06:18 +0000

    Initial Commit

---
+++ Dotted Circle View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dotted Circle View
+//
+//  Created by Hamish Knight on 12/03/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dotted Circle View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dotted Circle View
+//
+//  Created by Hamish Knight on 12/03/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
